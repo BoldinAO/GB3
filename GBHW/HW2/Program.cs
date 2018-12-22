a1e875c0e8f924cff38eb0ffbe688c59b86f4d1d
Anatoliy <boldinao@icloud.com>
2018-12-22 15:28:39 +0500

    Выполнено задание 2, 3

---
+++ GBHW/HW2/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace HW2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            test.Summ();
+            Console.ReadKey();
+        }
+
+        struct test
+        {
+            static int a { get; set; }
+            static int summ { get; set; }
+
+            public static void Summ()
+            {
+                a = 1;
+                do
+                {
+                    Ex();
+                } while(a!=0);
+                Console.Write("Сумма нечетных чисел = {0}", summ);
+            }
+
+            static void Ex()
+            {
+                try
+                {
+                    Console.Write("Введите число: ");
+                    a = int.Parse(Console.ReadLine());
+                    summ = a > 0 ? (a % 2 == 1 ? summ + a : summ) : summ;
+                }
+                catch
+                {
+                    Console.WriteLine("Необходимо ввести число!");
+                }
+            }
+        }
+    }
+}
---
+++ GBHW/HW3/Program.cs
@@ -0,0 +1,73 @@
+﻿using System;
+
+namespace HW3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Drob dr1 = new Drob();
+            Drob dr2 = new Drob();
+            Console.Write("Введите числитель 1 дроби: ");
+            dr1.Chislitel = int.Parse(Console.ReadLine());
+            Console.Write("Введите знаменатель 1 дроби: ");
+            dr1.Znamenatel = int.Parse(Console.ReadLine());
+            Console.Write("Введите числитель 2 дроби: ");
+            dr2.Chislitel = int.Parse(Console.ReadLine());
+            Console.Write("Введите знаменатель 2 дроби: ");
+            dr2.Znamenatel = int.Parse(Console.ReadLine());
+            Drob dr = Drob.Plus(dr1, dr2);
+            Console.WriteLine(dr.Chislitel.ToString() + "\n--");
+            Console.Write(dr.Znamenatel.ToString() + "\n--");
+            Console.ReadKey();
+        }
+    }
+
+    class Drob
+    {
+        int ch;
+        int zn;
+
+        public Drob() { }
+
+        public Drob(int ch, int zn)
+        {
+            this.ch = ch;
+            this.zn = zn;
+        }
+
+        public int Chislitel
+        {
+            get { return ch; }
+            set { ch = value; }
+        }
+
+        public int Znamenatel
+        {
+            get { return zn; }
+            set
+            {
+                if(value == 0)
+                    throw new ArgumentException(String.Format("Знаменатель не может быть равен {0}", value), "zn");
+                zn = value;
+            }
+        }
+
+        public static Drob Plus(Drob drob1, Drob drob2)
+        {
+            Drob dr = new Drob();
+
+            if (drob1.Znamenatel != drob2.Znamenatel)
+            {
+                dr.Chislitel = drob1.Chislitel * drob2.Znamenatel + drob2.Chislitel * drob1.Znamenatel;
+                dr.Znamenatel = drob1.Znamenatel * drob2.Znamenatel;
+            }
+            else
+            {
+                dr.Chislitel = drob1.Chislitel + drob2.Chislitel;
+                dr.Znamenatel = drob1.Znamenatel;
+            }
+            return dr;
+        }
+    }
+}
